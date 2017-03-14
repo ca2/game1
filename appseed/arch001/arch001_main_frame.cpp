2dd868b1598250ae83a36adaf74aa18a4c848294
camilo <camilo@ca2.email>
2017-03-14 10:48:20 +0000

    bilbo at veriverse i7 6700 32GB RAM Windows 10 Pro (booting also macOS Sierra, Hyper-V and Ubuntu 16.04 Unity Numix Theme Numix Circle Icons)

--- appseed/arch001/arch001_main_frame.cpp
+++ appseed/arch001/arch001_main_frame.cpp
@@ -7,7 +7,8 @@ namespace arch001
 
    main_frame::main_frame(::aura::application * papp):
       ::object(papp),
-      simple_frame_window(papp)
+      simple_frame_window(papp),
+      ::estamira::main_frame(papp)
    {
 
       //      m_dataid = "::core::arch001::main_frame";
